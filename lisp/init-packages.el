ffb3b9d94fa1669ce45dd4d4f5c402ce6a2f627d
Lanting Liu <ryulant1ng@gmail.com>
2025-08-03 18:17:42 +0800

    adjust themes

--- lisp/init-packages.el
+++ lisp/init-packages.el
@@ -17,6 +17,14 @@
   :config
   (setq company-transformers '(company-sort-by-occurrence)))
 
+(use-package company-box
+  :ensure t
+  :if window-system
+  :hook (company-mode . company-box-mode))
+
+(use-package neotree
+  :ensure t)
+
 (use-package editorconfig
   :ensure t
   :config
@@ -25,13 +33,15 @@
 (use-package powerline
   :ensure t
   :config
-  (powerline-default-theme))
+  (powerline-default-theme)
+  (setq powerline-default-separator nil))
 
 (use-package spaceline
   :ensure t
   :after powerline
   :config
-  (spaceline-emacs-theme))
+  (spaceline-emacs-theme)
+  (setq spaceline-minor-modes-separator " "))
 
 (use-package markdown-mode
   :ensure t)
